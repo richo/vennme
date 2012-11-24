79cdf273b2c5baec60aaedb27d916118cd68b2ab
richo <richo@psych0tik.net>
2012-11-25 04:20:07 +1100

    Normalize values to avoid errors

--- src/vennme/web.clj
+++ src/vennme/web.clj
@@ -12,17 +12,30 @@
 (defn toInt [i]
   (Integer/parseInt i))
 
+(def normalize
+  (fn [a b c]
+    (let [largest (max a b c)]
+      (let [to (/ 100 largest)]
+        (map (fn [x] (* x to)) [a b c])
+        )
+      )
+    )
+  )
+
 (defn venn-diagram [a b ab]
   ; (. GCharts newVennDiagram 100 80 60 30 30 30 10))
-  (. GCharts newVennDiagram
-     (toInt a)    ; Size of first circle
-     (toInt b)    ; Size of second circle
-     0            ; Size of third circle, 0 for no circle
-     (toInt ab)   ; Size of overlapping region
-     0            ; Overlaps between 3 circled charts, all zero for us
-     0
-     0
-     ))
+    (let [[na nb nab] (normalize (toInt a) (toInt b) (toInt ab))]
+      (. GCharts newVennDiagram
+         na    ; Size of first circle
+         nb    ; Size of second circle
+         0     ; Size of third circle, 0 for no circle
+         nab   ; Size of overlapping region
+         0     ; Overlaps between 3 circled charts, all zero for us
+         0
+         0
+         )
+      )
+    )
 
 (defpage "/up" {:keys [a b ab alabel blabel ablabel]}
          (let [chart (venn-diagram a b ab)]
