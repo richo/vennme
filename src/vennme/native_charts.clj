a1aa51d82425c92b2318f9a44faae19b08ebb327
richo <richo@psych0tik.net>
2012-11-29 04:59:13 +1100

    Draw our own images inline

--- src/vennme/native_charts.clj
+++ src/vennme/native_charts.clj
@@ -1,6 +1,7 @@
 (ns vennme.native-charts
   (:import [java.awt.image BufferedImage])
   (:import [java.awt.geom Ellipse2D])
+  (:import [java.awt Color])
   (:import [javax.imageio ImageIO])
   (:import [java.io ByteArrayOutputStream])
   (:import [java.io ByteArrayInputStream])
@@ -9,8 +10,19 @@
 (def img-width 1024)
 (def img-height 768)
 
+(def color1
+  (new Color 0.7 0.0 0.0 0.4)
+  )
+
+(def color2
+  (new Color 0.0 0.7 0.0 0.4)
+  )
+
 (defn new-buffered-image []
   (new BufferedImage img-width img-height (. BufferedImage TYPE_INT_ARGB)))
+
+(defn toInt [i]
+  (Integer/parseInt i))
 
 (defn new-ellipse [x y width height]
   (new java.awt.geom.Ellipse2D$Double x y width height))
@@ -18,8 +30,28 @@
 (defn native-venn-diagram [a b ab alabel blabel ablabel]
   (let [img (new-buffered-image)
         g   (. img createGraphics)
-        output (new ByteArrayOutputStream)]
-    (. g draw (new-ellipse 50 50 25 25))
+        output (new ByteArrayOutputStream)
+        ia  (toInt a)
+        ib  (toInt b)
+        iab (toInt ab)]
+
+    (let [
+          left-circle (new-ellipse (- (+ (/ img-width 2) iab) (* ia 2)) (- (/ img-height 2) ia)  (* ia 2) (* ia 2))
+          right-circle (new-ellipse (- (/ img-width 2) iab) (- (/ img-height 2) ib) (* ib 2) (* ib 2))
+         ]
+      (. g draw left-circle)
+      (. g setColor color1)
+      (. g fill left-circle)
+      (. g draw right-circle)
+      (. g setColor color2)
+      (. g fill right-circle)
+
+      (. g setColor Color/BLACK)
+      (. g drawString alabel (- (+ (/ img-width 2) iab) (* ia 2)) (/ img-height 2))
+      (. g drawString blabel (+ (/ img-width 2) iab) (/ img-height 2))
+      (. g drawString ablabel (- (/ img-width 2) iab) (/ img-height 2))
+      )
+
     (ImageIO/write img "png" output)
     (new ByteArrayInputStream (.toByteArray output))
     )
